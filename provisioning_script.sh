830d1b8cfde41ea2c85dffdd3bed59e3fd238a86
Jan <102370430+Flash1709@users.noreply.github.com>
2025-03-10 12:26:16 +0100

    Update provisioning_script.sh

--- provisioning_script.sh
+++ provisioning_script.sh
@@ -12,18 +12,5 @@ set -eo pipefail
 # Install your packages
 pip install polars, pandas, catboost
 
-# Download some useful files
-wget -P "${WORKSPACE}/" https://example.org/my-application.tar.gz
-tar xvf ${WORKSPACE}/my-application.tar.gz"
-
-# Set up any additional services
-echo "my-supervisor-config" > /etc/supervisor/conf.d/my-application.conf
-echo "my-supervisor-wrapper" > /opt/supervisor-scripts/my-application.sh
-chmod +x /opt/supervisor-scripts/my-application.sh
-
-# Reconfigure the instance portal
-rm -f /etc/portal.yaml
-export PORTAL_CONFIG="localhost:1111:11111:/:Instance Portal|localhost:1234:11234:/:My Application"
-
-# Reload Supervisor
-supervisorctl reload
+# Clone the Git repository (replace with the actual repo URL)
+git clone https://github.com/your-repo/your-project.git
